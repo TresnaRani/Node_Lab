fc69cd7cf98768f1b88deac6b74ea66b771bdf35
tresna <tresna312@gmail.com>
2023-12-04 07:06:57 +0600

    module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
-const s1 = require('./student');
-console.log(s1.getName());
-console.log(s1.getID());
-console.log(s1.cgpa);
+//const s1 = require('./student');
+const{getName} = require('./student');
+console.log(getName());
+//console.log(s1.getName());
+//console.log(s1.getID());
+//console.log(s1.cgpa);
