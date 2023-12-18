a150ee771da8110fabef89bdedc80190180fb940
tresna <tresna312@gmail.com>
2023-12-18 06:31:04 +0600

    os

--- Function_System/index.js
+++ Function_System/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 
-fs.writeFile('demo1.txt',"My name is Tresna",(err) =>{
+//fs.writeFile('demo1.txt',"My name is Tresna",(err) =>{
+    //fs.appendFile('demo1.txt',"I am from tmss engg. college",(err) =>{
+    fs.readFile('demo1.text','utf-8',(err,data)=>{
     if(err){
         console.log(err);
     }
     else{
-        console.log("successful");
+        console.log("data");
     }
 })
